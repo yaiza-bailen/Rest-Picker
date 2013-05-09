412659ee46d8eba3a12e1282e2def94a44b7ecca
Yaiza Bailen <yaiza.bailen@gmail.com>
2013-05-09 10:42:22 +0200

    Displaying number of restaurants per city in the choose_town site.

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from restaurants.models import Restaurant, Town 
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 
 # Create your views here.
 def choose_town(request, food_id):
-    print food_id
+
     rest_list = Restaurant.objects.filter(food=food_id)
     town_list=[]
     for r in rest_list:
-        c = Town.objects.get(id=r.town.id)
-        if c not in town_list:
-            town_list.append(c)
+        t = Town.objects.get(id=r.town.id)
+        if t not in town_list:
+            rests = Restaurant.objects.filter(food__id=food_id, town__id=t.id)
+            town_list.append({'town':t, 'count':len(rests)})
     return render(request, 'restaurants/choose_town.html', {'food_id':food_id, 'town_list':town_list})
 
 def choose_restaurant(request,food_id,town_id):
@@ -26,14 +25,16 @@ def restaurant(request, rest_id, message=''):
 
 def vote(request, rest_id):
     rest = get_object_or_404(Restaurant, pk=rest_id)
+
     votes = rest.votes
     submitted = request.POST['note_utilisateur']
     average = (votes + int(submitted))/2
-    print 'votes',rest.votes
-    print 'submitted',request.POST['note_utilisateur']
-    print 'average',average
+    rest.votes = average
+    rest.save()
 
     rest_menu = rest.menu.split(';')
     rest_menu = [e for e in rest_menu if e not in ('',' ')]
+
     message='Thanks for your vote!'
+
     return render(request, 'restaurants/restaurant.html', {'restaurant':rest, 'rest_menu':rest_menu, 'message':message})
