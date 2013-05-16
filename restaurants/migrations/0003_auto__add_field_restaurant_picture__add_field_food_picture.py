3a6e529c69f3851e95f1f90c7adfff4974041b9a
Yaiza Bailen <yaiza.bailen@gmail.com>
2013-05-16 14:28:02 +0200

    Rest Picker application complete!

---
+++ restaurants/migrations/0003_auto__add_field_restaurant_picture__add_field_food_picture.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Restaurant.picture'
+        db.add_column('restaurants_restaurant', 'picture',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True),
+                      keep_default=False)
+
+        # Adding field 'Food.picture'
+        db.add_column('restaurants_food', 'picture',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Restaurant.picture'
+        db.delete_column('restaurants_restaurant', 'picture')
+
+        # Deleting field 'Food.picture'
+        db.delete_column('restaurants_food', 'picture')
+
+
+    models = {
+        'restaurants.food': {
+            'Meta': {'object_name': 'Food'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'})
+        },
+        'restaurants.restaurant': {
+            'Meta': {'object_name': 'Restaurant'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'desc': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'food': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurants.Food']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'menu': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
+            'post_code': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'town': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurants.Town']"}),
+            'votes': ('django.db.models.fields.IntegerField', [], {'default': '4'}),
+            'web': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'restaurants.town': {
+            'Meta': {'object_name': 'Town'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['restaurants']
---
+++ restaurants/migrations/0005_auto__add_field_restaurant_gmap_url.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Restaurant.gmap_url'
+        db.add_column('restaurants_restaurant', 'gmap_url',
+                      self.gf('django.db.models.fields.CharField')(max_length=200, null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Restaurant.gmap_url'
+        db.delete_column('restaurants_restaurant', 'gmap_url')
+
+
+    models = {
+        'restaurants.food': {
+            'Meta': {'object_name': 'Food'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'})
+        },
+        'restaurants.restaurant': {
+            'Meta': {'object_name': 'Restaurant'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'desc': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'food': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurants.Food']"}),
+            'gmap_url': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'map': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
+            'menu': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
+            'post_code': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'town': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['restaurants.Town']"}),
+            'votes': ('django.db.models.fields.IntegerField', [], {'default': '4'}),
+            'web': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'restaurants.town': {
+            'Meta': {'object_name': 'Town'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['restaurants']
